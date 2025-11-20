0c65f5581b5af330f38ffc39ee7828b7c3f6c583
ferky36 <ferky36@users.noreply.github.com>
2025-11-20 10:56:45 +0000

    deploy: b741b94a8dd2fdaf0203dcbd65f6e70742f803a4

--- config.js
+++
@@ -1,2 +0,0 @@
-window.__SUPABASE_URL='https://edyneqtxtxdweczvcvvt.supabase.co';
-window.__SUPABASE_ANON='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkeW5lcXR4dHhkd2VjenZjdnZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzNzM1MjgsImV4cCI6MjA3MTk0OTUyOH0.J_tYzbZ6fM4hw8QSiOaVcQwnPkUNC-uAZKe7W5i81DY';
